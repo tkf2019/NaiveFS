cd0fa2942aa6c7cbca732b3daad2d2b3dd385c78
田凯夫 <tkf19@mails.tsinghua.edu.cn>
2022-05-07 15:35:04 +0800

    [Build] Run fuse example

---
+++ include/disk.h
@@ -0,0 +1,6 @@
+#ifndef NAIVEFS_INCLUDE_DISK_H_
+#define NAIVEFS_INCLUDE_DISK_H_
+
+namespace naivefs {}
+
+#endif
---
+++ include/inode.h
@@ -0,0 +1,8 @@
+#ifndef NAIVEFS_INCLUDE_INODE_H_
+#define NAIVEFS_INCLUDE_INODE_H_
+
+namespace naivefs {
+  
+}
+
+#endif
